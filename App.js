dab2947d3f7478a5e784d081bb24104550fba250
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-16 21:18:36 -0700

    react-native-maps

--- App.js
+++ App.js
@@ -1,34 +1,13 @@
-import { useState, useEffect } from 'react';
-import MapView, { Polyline } from 'react-native-maps';
-import { Button, StyleSheet, View } from 'react-native';
-
-import * as Location from 'expo-location';
+// MapView
+// https://docs.expo.dev/versions/latest/sdk/map-view/
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}>
-        <Polyline
-          coordinates={[
-            { latitude: 37.8025259, longitude: -122.4351431 },
-            { latitude: 37.7896386, longitude: -122.421646 },
-            { latitude: 37.7665248, longitude: -122.4161628 },
-            { latitude: 37.7734153, longitude: -122.4577787 },
-            { latitude: 37.7948605, longitude: -122.4596065 },
-            { latitude: 37.8025259, longitude: -122.4351431 },
-          ]}
-          strokeColor='#000' // fallback for when `strokeColors` is not supported by the map-provider
-          strokeColors={[
-            '#7F0000',
-            '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
-            '#B24112',
-            '#E5845C',
-            '#238C23',
-            '#7F0000',
-          ]}
-          strokeWidth={6}
-        />
-      </MapView>
+      <MapView style={styles.map} />
     </View>
   );
 }
