cee898e1b5630f8314b6a31fec25ecdb35bb9a93
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-16 21:19:47 -0700

    pinpoint accuracy

--- App.js
+++ App.js
@@ -1,44 +1,53 @@
 import { useState, useEffect } from 'react';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { Button, StyleSheet, View } from 'react-native';
 
 import * as Location from 'expo-location';
 
 export default function App() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+    console.log(location);
+  };
+
+  useEffect(() => {
+    userLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}>
-        <Polyline
-          coordinates={[
-            { latitude: 37.8025259, longitude: -122.4351431 },
-            { latitude: 37.7896386, longitude: -122.421646 },
-            { latitude: 37.7665248, longitude: -122.4161628 },
-            { latitude: 37.7734153, longitude: -122.4577787 },
-            { latitude: 37.7948605, longitude: -122.4596065 },
-            { latitude: 37.8025259, longitude: -122.4351431 },
-          ]}
-          strokeColor='#000' // fallback for when `strokeColors` is not supported by the map-provider
-          strokeColors={[
-            '#7F0000',
-            '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
-            '#B24112',
-            '#E5845C',
-            '#238C23',
-            '#7F0000',
-          ]}
-          strokeWidth={6}
+      <MapView
+        style={styles.map}
+        region={mapRegion}
+      >
+        <Marker
+          coordinate={mapRegion}
+          title='Marker'
         />
       </MapView>
+      <Button
+        title='Get Location'
+        onPress={userLocation}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-});
