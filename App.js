16a9100b1abfefce56f63fff09024de5151821b9
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-16 21:17:29 -0700

    Revise

--- App.js
+++ App.js
@@ -1,34 +1,116 @@
-import { useState, useEffect } from 'react';
-import MapView, { Polyline } from 'react-native-maps';
-import { Button, StyleSheet, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
-import * as Location from 'expo-location';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+
+import { Pedometer } from 'expo-sensors';
+
+import CircularProgress from 'react-native-circular-progress-indicator';
 
 export default function App() {
+  const [PedomaterAvailability, SetPedomaterAvailability] = useState('');
+  const [StepCount, SetStepCount] = useState(0);
+  var WindowHeight = Dimensions.get('window').height;
+  var Dist = StepCount / 1300;
+  var DistanceCovered = Dist.toFixed(4);
+  var cal = DistanceCovered * 60;
+  var caloriesBurnt = cal.toFixed(4);
+
+  useEffect(() => {
+    subscribe();
+  }, []);
+
+  subscribe = () => {
+    const subscription = Pedometer.watchStepCount((result) => {
+      SetStepCount(result.steps);
+    });
+
+    Pedometer.isAvailableAsync().then(
+      (result) => {
+        SetPedomaterAvailability(String(result));
+      },
+
+      (error) => {
+        SetPedomaterAvailability(error);
+      }
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}>
-        <Polyline
-          coordinates={[
-            { latitude: 37.8025259, longitude: -122.4351431 },
-            { latitude: 37.7896386, longitude: -122.421646 },
-            { latitude: 37.7665248, longitude: -122.4161628 },
-            { latitude: 37.7734153, longitude: -122.4577787 },
-            { latitude: 37.7948605, longitude: -122.4596065 },
-            { latitude: 37.8025259, longitude: -122.4351431 },
-          ]}
-          strokeColor='#000' // fallback for when `strokeColors` is not supported by the map-provider
-          strokeColors={[
-            '#7F0000',
-            '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
-            '#B24112',
-            '#E5845C',
-            '#238C23',
-            '#7F0000',
-          ]}
-          strokeWidth={6}
-        />
-      </MapView>
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require('./assets/runningFinal.jpg')}
+        resizeMode='cover'
+      >
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text style={styles.headingDesign}>
+            Is Pedometer available on the device : {PedomaterAvailability}
+          </Text>
+        </View>
+
+        <View style={{ flex: 3 }}>
+          <CircularProgress
+            value={StepCount}
+            maxValue={6500}
+            radius={210}
+            textColor={'#ecf0f1'}
+            activeStrokeColor={'#f39c12'}
+            inActiveStrokeColor={'#9b59b6'}
+            inActiveStrokeOpacity={0.5}
+            inActiveStrokeWidth={40}
+            activeStrokeWidth={40}
+            title={'Step Count'}
+            titleColor={'#ecf0f1'}
+            titleStyle={{ fontWeight: 'bold' }}
+          />
+        </View>
+
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <View style={{ flex: 1 }}>
+            <Text
+              style={[
+                styles.textDesign,
+
+                { paddingLeft: 20, marginLeft: '23%' },
+              ]}
+            >
+              Target : 6500 steps(5kms)
+            </Text>
+          </View>
+
+          <View style={{ flex: 1 }}>
+            <Text
+              style={[
+                styles.textDesign,
+
+                { width: '93%', paddingLeft: 20, marginLeft: '-3.5%' },
+              ]}
+            >
+              Distance Covered : {DistanceCovered} km
+            </Text>
+          </View>
+
+          <View style={{ flex: 1 }}>
+            <Text
+              style={[
+                styles.textDesign,
+                { paddingLeft: 10, marginLeft: '23%' },
+              ]}
+            >
+              Calories Burnt : {caloriesBurnt}
+            </Text>
+          </View>
+
+          <StatusBar style='auto' />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -36,9 +118,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
   },
-  map: {
-    width: '100%',
-    height: '100%',
+  headingDesign: {
+    backgroundColor: 'rgba(155, 89, 182,0.5)',
+    alignSelf: 'center',
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    fontFamily: 'Papyrus',
+  },
+  textDesign: {
+    backgroundColor: 'rgba(155, 89, 182,0.5)',
+    height: 50,
+    width: '85%',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 20,
+    overflow: 'hidden',
+    fontSize: 25,
+    color: 'white',
+    fontWeight: 'bold',
+    fontFamily: 'Papyrus',
   },
 });
