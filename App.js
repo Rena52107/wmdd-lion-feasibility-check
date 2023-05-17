d36f9ef58dc293e866ab69202d09df9d6f028cb8
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-16 17:25:59 -0700

    Add Pinpoint Accuracy

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
-// MapView
-// https://docs.expo.dev/versions/latest/sdk/map-view/
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { Button, StyleSheet, View } from 'react-native';
+
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+    console.log(location);
+  };
+
+  useEffect(() => {
+    userLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        region={mapRegion}
+      >
+        <Marker
+          coordinate={mapRegion}
+          title='Marker'
+        />
+      </MapView>
+      <Button
+        title='Get Location'
+        onPress={userLocation}
+      />
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # wmdd-lion-feasibility-check
 
-Step Counter (Pedometer)
+### Step Counter (Pedometer)
+
 React Native Tutorial for Beginners - Build a Step Counter (Pedometer) App [2022]
 https://www.youtube.com/watch?v=RaSyX6COTDk
+
+### MapView
+
+https://docs.expo.dev/versions/latest/sdk/map-view/
+
+### Map View with Pinpoint Accuracy
+(react-native-maps, expo-location)
+React Native Map View With Pinpoint Accuracy | React Native & Expo Project Tutorial | JS Code
+https://youtu.be/su5v_Gg9Sxk
